ed528a47e65f27af4a2d664ae21f0f3b21b63083
Mileto Di Marco <mileto.di.marco@howest.be>
2021-10-01 11:35:02 +0200

    Add View model for Getgames method

---
+++ cu.ViewModels.Lesvoorbeeld/Controllers/GamesController.cs
@@ -0,0 +1,44 @@
+﻿using cu.ViewModels.Lesvoorbeeld.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.Oefening.Games.Core;
+
+namespace cu.ViewModels.Lesvoorbeeld.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly GameRepository _gameRepository;
+
+        public GamesController()
+        {
+            _gameRepository = new GameRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetGames()
+        {
+            //declare view model
+            GamesGetGamesViewModel getGamesViewModel
+                = new GamesGetGamesViewModel();
+            getGamesViewModel.Titles = new List<string>();
+            //get games from repo/database
+           foreach(var game in _gameRepository.GetGames())
+            {
+                getGamesViewModel.Titles.Add(game?.Title ?? "<NoTitle>");
+            }
+            ViewBag.PageTitle = "Our Games";
+            //send to the view
+            return View(getGamesViewModel);
+        }
+
+        public IActionResult GameInfo(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ cu.ViewModels.Lesvoorbeeld/ViewModels/GamesGetGamesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cu.ViewModels.Lesvoorbeeld.ViewModels
+{
+    public class GamesGetGamesViewModel
+    {
+        public List<string> Titles { get; set; }
+    }
+}
