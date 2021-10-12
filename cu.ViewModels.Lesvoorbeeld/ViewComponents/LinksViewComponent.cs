bbca2285e02ac4a5022d09ffb28c737ec3ddba88
Mileto Di Marco <mileto.di.marco@howest.be>
2021-10-12 16:09:57 +0200

    Add view component for links

---
+++ cu.ViewModels.Lesvoorbeeld/ViewComponents/LinksViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using cu.ViewModels.Lesvoorbeeld.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cu.ViewModels.Lesvoorbeeld.ViewComponents
+{
+    [ViewComponent(Name = "LinksComponent")]
+    public class LinksViewComponent : ViewComponent
+    {
+        
+        private readonly List<LinkViewComponentViewModel>
+            _linksViewComponentViewModel = new List<LinkViewComponentViewModel>();
+
+      
+        public IViewComponentResult Invoke()
+        {
+            _linksViewComponentViewModel.Add(new LinkViewComponentViewModel
+            {
+                Url = "http://www.gazetta.it",
+                Name = "Gazzeta"
+            });
+            _linksViewComponentViewModel.Add(new LinkViewComponentViewModel
+            {
+                Url = "http://www.github.com",
+                Name = "Github"
+            });
+            return View(_linksViewComponentViewModel);
+        }
+    }
+}
---
+++ cu.ViewModels.Lesvoorbeeld/ViewModels/LinkViewComponentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cu.ViewModels.Lesvoorbeeld.ViewModels
+{
+    public class LinkViewComponentViewModel
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+    }
+}
